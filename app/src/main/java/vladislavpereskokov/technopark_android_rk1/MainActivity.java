87caec70a6b92a6920773fae72bcad0050805d65
vladpereskokov <vladpereskokov@mail.ru>
2017-10-03 18:19:23 +0300

    add second actirvity

--- app/src/main/java/vladislavpereskokov/technopark_android_rk1/MainActivity.java
+++ app/src/main/java/vladislavpereskokov/technopark_android_rk1/MainActivity.java
@@ -1,7 +1,9 @@
 package vladislavpereskokov.technopark_android_rk1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.square).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                final Intent intent = new Intent(MainActivity.this, CalcActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.factorial).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                final Intent intent = new Intent(MainActivity.this, CalcActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
